ae31d13a0beeecc33af542f93d811f1ed1e6bfb2
Alelizzt <alejandrolistc@hotmail.com>
2023-09-19 17:30:14 -0500

    Add validations on Employee
    for date and time format especially

--- Backend/HumanTalent/src/main/java/com/humantalent/domain/model/employee/Employee.java
+++ Backend/HumanTalent/src/main/java/com/humantalent/domain/model/employee/Employee.java
@@ -1,9 +1,10 @@
 package com.humantalent.domain.model.employee;
 
-import com.humantalent.domain.model.EmailGenerator;
+import com.humantalent.domain.utils.EmailGenerator;
 import com.humantalent.domain.model.person.Person;
+import com.humantalent.domain.utils.LocalDateTimeConverter;
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.time.LocalDateTime;
@@ -13,17 +14,44 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "employees")
+@PrimaryKeyJoinColumn(name = "person_id")
 public class Employee extends Person {
 
+    @Column(name = "work_area")
+    @Enumerated(EnumType.STRING)
     private EmployeeWorkArea workArea;
+
     private boolean state;
+
+    @Column(name = "reg_datetime")
+    @Convert(converter = LocalDateTimeConverter.class) // DD/MM/YYYY HH:mm:ss
     private LocalDateTime registrationDateAndTime;
+
+    @Column(name = "entry_date")
+    @Convert(converter = LocalDateTimeConverter.class) // DD/MM/YYYY HH:mm:ss
     private LocalDateTime entryDate;
+
+    @Column(name = "email", length = 300)
     private String email;
 
+    @Transient // No persistence on DB
     private EmailGenerator emailGenerator;
 
     public void generateAndSetEmail() {
         this.email = emailGenerator.generateEmail(this);
     }
+
+    @PrePersist
+    private void beforePersistence(){
+        this.entryDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void afterUpdate(){
+        this.registrationDateAndTime = LocalDateTime.now();
+    }
+
+
 }
