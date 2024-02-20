6584546b324ac3addb40d175c91b9fd28318b8b8
Alelizzt <alejandrolistc@hotmail.com>
2024-02-20 15:24:32 -0500

    Add basic components

--- Frontend/HumanTalent/src/app/app.routes.ts
+++ Frontend/HumanTalent/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: EmployeeComponent},
+    {path: '**', component: ErrorComponent}
+];
