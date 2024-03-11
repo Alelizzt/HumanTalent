13cbd8a9fb6b46a57ece89f53106862959d482d5
Alelizzt <alejandrolistc@hotmail.com>
2024-03-11 16:27:47 -0500

    Configurations to test repositories
    using H2 has testing database

---
+++ Backend/HumanTalent/src/test/java/com/humantalent/adapters/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.humantalent.adapters.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryTest {
+
+    @Test
+    void orderByEmail() {
+    }
+
+    @Test
+    void orderByWorkArea() {
+    }
+
+    @Test
+    void orderByState() {
+    }
+
+    @Test
+    void orderByRegDateTime() {
+    }
+
+    @Test
+    void orderByEntryDate() {
+    }
+
+    @Test
+    void findByEmailContaining() {
+    }
+
+    @Test
+    void existsByEmail() {
+    }
+
+    @Test
+    void findByState() {
+    }
+}
