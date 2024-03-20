a4d1b2bfa95a46d35a8a2f1703347e3507d029cf
Alelizzt <alejandrolistc@hotmail.com>
2024-03-20 18:02:58 -0500

    Document Employee Schema with subtype for Swagger

--- Backend/HumanTalent/src/main/java/com/humantalent/domain/model/employee/Employee.java
+++ Backend/HumanTalent/src/main/java/com/humantalent/domain/model/employee/Employee.java
@@ -1,8 +1,11 @@
 package com.humantalent.domain.model.employee;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.humantalent.domain.utils.EmailGenerator;
 import com.humantalent.domain.model.person.Person;
 import com.humantalent.domain.utils.LocalDateTimeConverter;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,26 +22,39 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "employees")
 @PrimaryKeyJoinColumn(name = "person_id")
+@Schema(oneOf = Person.class, description = "Tipo de persona")
 public class Employee extends Person {
 
+    @Schema(description = "Área de trabajo del empleado", examples = {
+            "ADMINISTRATION",
+            "FINANCE",
+            "PROCUREMENT",
+            "INFRASTRUCTURE",
+            "OPERATIONS",
+            "HUMAN_RESOURCES",
+            "MISCELLANEOUS_SERVICES"})
     @Column(name = "work_area")
     @Enumerated(EnumType.STRING)
     private EmployeeWorkArea workArea;
 
+    @Schema(description = "Estado actual del empleado")
     @Column(nullable = false)
     private boolean state = Boolean.TRUE;
 
+    @Hidden
     @Column(name = "reg_datetime")
     @Convert(converter = LocalDateTimeConverter.class) // DD/MM/YYYY HH:mm:ss
     private LocalDateTime regDateTime;
 
+    @Schema(description = "Fecha de ingreso del empleado", example = "2024-02-12")
     @Column(name = "entry_date")
-    //@Convert(converter = LocalDateTimeConverter.class) // DD/MM/YYYY HH:mm:ss
     private LocalDate entryDate;
 
+    @Hidden
     @Column(name = "email", length = 300)
     private String email;
 
+    @Hidden
     @Transient
     EmailGenerator emailGenerator;
 
@@ -50,7 +66,6 @@ public class Employee extends Person {
 
     @PrePersist
     private void beforePersistence(){
-        //this.entryDate = LocalDate.now();
         this.regDateTime = LocalDateTime.now();
     }
 
