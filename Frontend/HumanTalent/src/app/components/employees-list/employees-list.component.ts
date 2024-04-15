eb7b46d158aec7b81973e91e04f644589d99681a
Alelizzt <alejandrolistc@hotmail.com>
2024-04-15 16:10:46 -0500

    Add filtering and sorting employees

--- Frontend/HumanTalent/src/app/components/employees-list/employees-list.component.ts
+++ Frontend/HumanTalent/src/app/components/employees-list/employees-list.component.ts
@@ -6,10 +6,9 @@ import { Employee } from '../../models/employee.model';
 @Component({
   selector: 'app-employee',
   templateUrl: './employees-list.component.html',
-  styleUrl: './employees-list.component.css'
+  styleUrl: './employees-list.component.css',
 })
 export class EmployeesListComponent {
-
   employees?: Employee[];
   currentEmployee: Employee = {};
   confirmed: any;
@@ -17,48 +16,86 @@ export class EmployeesListComponent {
   currentPage: number = 0;
   totalItems: number = 0;
   totalPages: number = 0;
+  filter: string = '';
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _employeeService: EmployeesService,
-    
-    ){ 
-      this.confirmed = null;
-     }
+    private _employeeService: EmployeesService
+  ) {
+    this.confirmed = null;
+  }
 
-  ngOnInit(){
-    this.retrieveEmployees();
+  ngOnInit() {
+    this._route.queryParams.subscribe(params => {
+      this.filter = params['filter'] || ''; // Usar el valor del filtro de los parámetros de la URL, o cadena vacía si no hay filtro
+      this.retrieveEmployees();
+    });
   }
 
   retrieveEmployees() {
     this._employeeService.getPaginatedData(this.currentPage).subscribe({
-          next: (response: any) => {
-          this.employees = response.data.content;
-          this.currentPage = response.data.pageable.pageNumber;
-          this.totalItems = response.data.totalElements;
-          this.totalPages = response.data.totalPages;
-        },
-          error: (e) => {
-            console.error(e);
-          }
+      next: (response: any) => {
+        this.employees = response.data.content;
+        this.currentPage = response.data.pageable.pageNumber;
+        this.totalItems = response.data.totalElements;
+        this.totalPages = response.data.totalPages;
+      },
+      error: (e) => {
+        console.error(e);
+      },
     });
   }
 
-  onPageChange(page:number) {
+  retrieveSortedEmployees() {
+    this._employeeService.getPaginatedSortedData(this.filter).subscribe({
+      next: (response: any) => {
+        this.employees = response.data.content;
+        this.currentPage = response.data.pageable.pageNumber;
+        this.totalItems = response.data.totalElements;
+        this.totalPages = response.data.totalPages;
+      },
+      error: (e) => {
+        console.error(e);
+      },
+    });
+  }
+
+  applyFilter(filterValue: string) {
+    this.filter = filterValue;
+    /*this._router.navigate([], {
+      relativeTo: this._route,
+      queryParams: { filter: filterValue }, // Agregar el filtro como parámetro de la URL
+      queryParamsHandling: 'merge', // Mantener otros parámetros de la URL
+    });*/
+
+    this.filter = filterValue;
+
+    if (filterValue) {
+      this.retrieveSortedEmployees();
+    } else {
+      this.retrieveEmployees();
+    }
+  }
+
+  onPageChange(page: number) {
     this.currentPage = page;
-    this.retrieveEmployees();
+    if (this.filter) {
+      this.retrieveSortedEmployees();
+    } else {
+      this.retrieveEmployees();
+    }
   }
 
   goToPreviousPage() {
-    if(this.currentPage >= 1) {
+    if (this.currentPage >= 1) {
       this.currentPage--;
       this.retrieveEmployees();
     }
   }
 
   goToNextPage() {
-    if(this.currentPage <= this.totalPages) {
+    if (this.currentPage <= this.totalPages) {
       this.currentPage++;
       this.retrieveEmployees();
     }
@@ -66,30 +103,30 @@ export class EmployeesListComponent {
 
   goToPage(page: string) {
     const pageNumber = parseInt(page, 10);
-    if(pageNumber && pageNumber >= 1 && pageNumber <= this.totalPages && pageNumber !== this.currentPage) {
+    if (
+      pageNumber &&
+      pageNumber >= 1 &&
+      pageNumber <= this.totalPages &&
+      pageNumber !== this.currentPage
+    ) {
       this.currentPage = pageNumber;
       this.retrieveEmployees();
     }
   }
 
-  goEmployeeDetail(id : number) {
-    this._router.navigate(
-      ['/employee', id]
-    )
+  goEmployeeDetail(id: number) {
+    this._router.navigate(['/employee', id]);
   }
 
   goEmployeeEdit(id: number) {
-    
-    this._router.navigate(
-      ['/edit-employee', id]
-    )
+    this._router.navigate(['/edit-employee', id]);
   }
-  
+
   gotoTop() {
-    window.scroll({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'smooth' 
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
     });
   }
 
@@ -105,13 +142,10 @@ export class EmployeesListComponent {
     this._employeeService.deleteEmployee(id).subscribe({
       next: (response: any) => {
         this.retrieveEmployees();
-      },error: (e) => {
-            console.error(e);
-          }
+      },
+      error: (e) => {
+        console.error(e);
+      },
     });
   }
-
-
-
-
 }
--- Frontend/HumanTalent/src/app/services/employees/employees.service.ts
+++ Frontend/HumanTalent/src/app/services/employees/employees.service.ts
@@ -5,54 +5,58 @@ import { Observable, catchError, map, retry } from 'rxjs';
 import { Employee } from '../../models/employee.model';
 import { DatePipe } from '@angular/common';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeesService {
+  private url: string = GLOBAL.url;
 
-  private url :string = GLOBAL.url;
+  constructor(private _http: HttpClient) {}
 
-  constructor( private _http: HttpClient) {
-   }
-
-   getAll(): Observable<Employee[]> {
+  getAll(): Observable<Employee[]> {
     return this._http.get<Employee[]>(`${this.url}employees`);
-   }
+  }
 
-   addEmployee(employee: Employee): Observable<any> {
+  addEmployee(employee: Employee): Observable<any> {
     let json = JSON.stringify(employee);
-    const headers = new HttpHeaders().set('Vary','Origin')
-    .set('Vary','Access-Control-Request-Method')
-    .set('Vary','Access-Control-Request-Headers')
-    .set('Content-Type','application/json');
+    const headers = new HttpHeaders()
+      .set('Vary', 'Origin')
+      .set('Vary', 'Access-Control-Request-Method')
+      .set('Vary', 'Access-Control-Request-Headers')
+      .set('Content-Type', 'application/json');
 
-    return this._http.post<Employee>(this.url+'employees',json, {'headers': headers});
+    return this._http.post<Employee>(this.url + 'employees', json, {
+      headers: headers,
+    });
   }
 
   getPaginatedData(page: number): Observable<Employee> {
-    const params = new HttpParams()
-    .set('page', page.toString())
+    const params = new HttpParams().set('page', page.toString());
+    return this._http.get<Employee>(this.url + 'employees', { params });
+  }
 
-    return this._http.get<Employee>(this.url+'employees',{params});
+  getPaginatedSortedData(filter: string) {
+    return this._http.get<Employee>(this.url + 'employees/'+filter);
   }
 
   getEmployee(id: number): Observable<Employee> {
-    return this._http.get<Employee>(`${this.url}employees/id/`+id);
+    return this._http.get<Employee>(`${this.url}employees/id/` + id);
   }
 
-  editEmployee(id: number, employee:Employee) {
+  editEmployee(id: number, employee: Employee) {
     let json = JSON.stringify(employee);
-    const headers = new HttpHeaders().set('Vary','Origin')
-    .set('Vary','Access-Control-Request-Method')
-    .set('Vary','Access-Control-Request-Headers')
-    .set('Content-Type','application/json');
+    const headers = new HttpHeaders()
+      .set('Vary', 'Origin')
+      .set('Vary', 'Access-Control-Request-Method')
+      .set('Vary', 'Access-Control-Request-Headers')
+      .set('Content-Type', 'application/json');
 
-    return this._http.put<Employee>(this.url+'employees/'+id, json, {'headers': headers})
+    return this._http.put<Employee>(this.url + 'employees/' + id, json, {
+      headers: headers,
+    });
   }
 
   deleteEmployee(id: number) {
-    return this._http.delete(this.url+'employees/'+id);
+    return this._http.delete(this.url + 'employees/' + id);
   }
-
 }
