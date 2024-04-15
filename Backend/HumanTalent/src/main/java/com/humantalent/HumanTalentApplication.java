9eb225c309f003fd1230652383d86f0ee0a78f83
Alelizzt <alejandrolistc@hotmail.com>
2024-04-15 12:06:34 -0500

    Add origin on same port as project
    using static files in this project generated by Angular

--- Backend/HumanTalent/src/main/java/com/humantalent/HumanTalentApplication.java
+++ Backend/HumanTalent/src/main/java/com/humantalent/HumanTalentApplication.java
@@ -24,7 +24,7 @@ public class HumanTalentApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200");
+						.allowedOrigins("http://localhost:4200","http://localhost:9081");
 			}
 		};
 	}
