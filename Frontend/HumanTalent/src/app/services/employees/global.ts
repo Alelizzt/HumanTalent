29ad89702dc125af0585cc837b3b767433aa08af
Alelizzt <alejandrolistc@hotmail.com>
2024-05-02 16:45:39 -0500

    Add Compodoc to documentation

--- Frontend/HumanTalent/src/app/services/employees/global.ts
+++ Frontend/HumanTalent/src/app/services/employees/global.ts
@@ -1,3 +1,4 @@
 export var GLOBAL = {
-    url: 'http://192.168.1.41:9081/api/v1/talent/',
+    url: 'http://192.168.1.45:9081/human-talent/api/v1/talent/',
+    //url: 'http://127.0.0.1:9081/human-talent/api/v1/talent/',
 }
